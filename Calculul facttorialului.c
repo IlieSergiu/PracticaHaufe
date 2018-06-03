e95c5e64cb2e718431b8b317c99c28d883c7ef4a
IlieSergiu <39828397+IlieSergiu@users.noreply.github.com>
2018-06-03 13:17:43 +0300

    Create Calculul facttorialului.c

---
+++ Calculul facttorialului.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+int n,con;
+long fact=1;
+printf("\n n=");
+scanf("%d", &n);
+for(con=1;con<=n;con++)
+  fact*=cont;
+printf("\n %d!=%ld, n, fact);
+getch();
+}
