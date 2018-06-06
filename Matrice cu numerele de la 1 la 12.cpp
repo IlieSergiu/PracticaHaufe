f7c5b5e5705dc81c0e9add46d119c26357cba690
IlieSergiu <39828397+IlieSergiu@users.noreply.github.com>
2018-06-06 13:05:37 +0300

    Create Matrice cu numerele de la 1 la 12.cpp

---
+++ Matrice cu numerele de la 1 la 12.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+
+void main()
+{
+int t,i, num[3][4];
+
+for(t=0;t<3;++t)
+    for(i=0;i<4;++i)
+        num[t][i]=(t*4)+i+1;
+/*acum afiseaza*/
+for(t=0;t<3;++t){
+    for(i=0;i<4;++i)
+        cout<<num[t][i];
+        cout<<"\n";
+        }
+   }     
