687d642bb4d973ecc4e03da7080a944406977ae0
IlieSergiu <39828397+IlieSergiu@users.noreply.github.com>
2018-06-08 15:16:46 +0300

    Create suma a n numere prime.cpp

---
+++ suma a n numere prime.cpp
@@ -0,0 +1,23 @@
+#include<iostreamh>
+
+int n;
+
+void prim(int n)
+{
+int s=0,x=0,i,a=1;
+while(x<n)
+    {i=2;
+    while(i<=a/2 && a%i!=)
+        i++;
+        if(i>a/2)
+          {cout<<a<<endl;
+          x++;
+          s+=a;}
+          a++;}
+          cout<<" suma este "<<s<<endl;}
+          
+void main()
+{
+cout<<" Dati n= ";cin>>n;
+prim(n);
+}
